55345d54378f27e0410d3c28bca52a0fdbbe1f22
Edward-Chau <edwardchau517@gmail.com>
2024-10-26 01:17:09 +0800

    Update categoriesScreen.dart

--- lib/Screen/categoriesScreen.dart
+++ lib/Screen/categoriesScreen.dart
@@ -2,39 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:udemy_08/Widget/DrawerBar.dart';
 import 'package:udemy_08/Widget/categoriesItem.dart';
 import 'package:udemy_08/model/Model.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({ super.key, required this.mealList});
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({super.key, required this.mealList});
 
+  final List<Meal> mealList;
 
-    final List<Meal> mealList;
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+// class _CategoriesScreenState extends State<CategoriesScreen>
+//     with TickerProviderStateMixin {
+//   // AnimationController animationController= AnimationController(vsync: );
+//   late AnimationController animation01;
+//   @override
+//   void initState() {
+//     animation01 = AnimationController(
+//       vsync: this,
+//       duration: const Duration(seconds: 3),
+//       lowerBound: 0,
+//       upperBound: 1,
+//     );
+//     animation01.forward();
+//     super.initState();
+//   }
+
+//   @override
+//   void dispose() {
+//     animation01.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: const MainDrawerBar(),
+//       appBar: AppBar(
+//         title: const Text("Categories"),
+//       ),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: AnimatedBuilder(
+//               animation: animation01,
+//               child: CategoriesItem(mealList: widget.mealList),
+//               builder: (context, child) {
+//                 return Padding(
+//                   padding: EdgeInsets.only(top: 100 - animation01.value * 100),
+//                   child: child,
+//                 );
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  // AnimationController animationController= AnimationController(vsync: );
+
   @override
   Widget build(BuildContext context) {
-
-
-
-
     return Scaffold(
-      drawer:  const MainDrawerBar(),
+      drawer: const MainDrawerBar(),
       appBar: AppBar(
         title: const Text("Categories"),
       ),
       body: Column(
         children: [
-          // InkWell(
-          //   onTap: () async {
-          //     await launchUrl(
-          //         Uri.parse("https://www.instagram.com/niki._.cos/"));
-          //   },
-          //   child: const Text(
-          //     "niki ig",
-          //     style: TextStyle(color: Colors.white),
-          //   ),
-          // ),
           Expanded(
-            child: CategoriesItem(mealList: mealList,
-        
+            child: AnimatedSwitcher(
+              duration: const Duration(seconds: 3),
+              transitionBuilder: (child, animation) {
+                return AnimatedSlide(
+                    offset: Offset(0, 0), duration: Duration(seconds: 3),child: child,);
+              },
+              child: CategoriesItem(mealList: widget.mealList),
             ),
           ),
         ],
